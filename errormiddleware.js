0fd019da0e0863fe6be8426553f6ce9bb30ab879
manojkeer99 <keermanoj99@gmail.com>
2026-07-16 12:53:11 +0530

    Add files via upload
    Js

---
+++ errormiddleware.js
@@ -0,0 +1,37 @@
+/**
+ * middleware/errorMiddleware.js
+ */
+
+
+const errorMiddleware = (err, req, res, next) => {
+
+
+    const statusCode =
+
+    res.statusCode === 200 ? 500 : res.statusCode;
+
+
+
+    res.status(statusCode).json({
+
+
+        message: err.message,
+
+
+        stack:
+
+        process.env.NODE_ENV === "production"
+
+        ? null
+
+        : err.stack
+
+
+    });
+
+
+};
+
+
+
+export default errorMiddleware;
